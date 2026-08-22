4d9808174ad860edabe59e1283e5db0296267f69
Elmurod <elmurodismo1lov008@gmail.com>
2026-08-22 14:57:29 +0500

    Fix supabase fallback url for build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://hdwchioyzfctztmjzsrm.supabase.co";
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "sb_publishable_3CV3vTyg5hp5n2niK6oUpQ_BXRBvH55";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
