3c9d77c5fe80ff9bdb80c5ce0052c4576b141385
Elmurod <elmurodismo1lov008@gmail.com>
2026-08-22 15:01:48 +0500

    Add force-dynamic to bypass build prerender error

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { useState, useEffect, use } from "react";
 import Link from "next/link";
 import { supabase } from "../../../lib/supabase";
